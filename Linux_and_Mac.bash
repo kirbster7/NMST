5d5973c1c13d18dab4d3b70881125e8e2186d2dd
notkirb_ <itnojheals@gmail.com>
2023-01-20 18:27:10 -0700

    fix many errors

--- Linux_and_Mac.bash
+++ Linux_and_Mac.bash
@@ -4,7 +4,7 @@ read -p "Enter dedotated wam: " mem
 echo "Starting server..."
 while true
 do
-java -Xms$"mem"M -Xmx$"mem"M -jar server.jar
+java -Xms$"mem"M -Xmx$"mem"M -jar server.jar -nogui
 echo "Server has stopped. Server will restart in 5 seconds. [Hit CTRL+C to exit]"
 sleep 5
 echo "Restarting server..."
--- Windows.bat
+++ Windows.bat
@@ -4,7 +4,7 @@ echo ""
 set /p mem="Enter dedotated wam: "
 echo "Starting server..."
 :start
-java -Xms%mem%M -Xmx%mem%M -jar server.jar
+"C:\Program Files\Java\jre1.8.0_351\bin\javaw.exe" -Xms%mem%M -Xmx%mem%M -jar server.jar
 echo "Server has stopped. Server will restart in 5 seconds. [Hit CTRL+C to exit]"
 timeout /t 5 /nobreak
 echo "Restarting server..."
